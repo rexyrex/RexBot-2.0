328eecdcbc1e2c3bc3d8859b8a249afea768a94b
rexyrex <minhyung.kim1234@gmail.com>
2017-05-12 23:02:12 -0700

    Added test module

---
+++ RexBot2/Modules/Test.cs
@@ -0,0 +1,17 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RexBot2.Modules
+{
+    public class Test : ModuleBase<SocketCommandContext>
+    {
+        [Command("test")]
+        public async Task testCmd()
+        {
+            await Context.Channel.SendMessageAsync("success!");
+        }
+    }
+}
